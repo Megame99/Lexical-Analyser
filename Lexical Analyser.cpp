e33be7377d097eba3964902e07323842138584c2
Megan Machkouri <mach0079@algonquinlive.com>
2022-01-19 14:45:12 -0500

    Add project files.

---
+++ Lexical Analyser.cpp
@@ -0,0 +1,17 @@
+﻿// Lexical Analyser.cpp : Defines the entry point for the application.
+//
+
+#include "Lexical Analyser.h"
+
+
+
+int main()
+{
+	char carthageKeywords[20][10] = {
+		"auto","stop","char","eternal","loop",
+		"dec","el","func", "float","for","goto",
+		"if","int","big","register","yield","short",
+		"wild","while"
+	};
+	return 0;
+}
