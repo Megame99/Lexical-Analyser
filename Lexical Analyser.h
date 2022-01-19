3af4c1cef5890a06ddbcf602c17898b45c23c0af
Megan Machkouri <mach0079@algonquinlive.com>
2022-01-19 14:49:43 -0500

    Updated Header

--- Lexical Analyser.h
+++ Lexical Analyser.h
@@ -5,4 +5,12 @@
 
 #include <stdio.h>
 
+/*
+Subset of C – Abides to C language specification (especially regarding storage and memory of Datatypes) however  will contain additional 
+functionality and different naming convention.
+
+The goal of Carthage is to add some useful functionality to C. In other words, to make C less cumbersome and implement improvements.
+
+*/
+
 // TODO: Reference additional headers your program requires here.
